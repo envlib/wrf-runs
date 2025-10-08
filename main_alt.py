1b866b5c8b92ad2d50703546148fd06c21c579a8
Mike <mullenkamp1@gmail.com>
2025-10-08 15:49:58 +1300

    updated

---
+++ main_alt.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 15:09:38 2025
+
+@author: mike
+"""
+from download_nml_domain import dl_nml_domain
+from set_params import check_set_params
+from download_era5 import dl_era5
+from run_era5_to_int import run_era5_to_int
+from run_metgrid import run_metgrid
+from run_real import run_real
+from monitor_wrf import monitor_wrf
+
+import params
+
+import sentry_sdk
+
+########################################
+## Sentry
+sentry = params.file['sentry']
+
+if sentry['dsn'] != '':
+    sentry_sdk.init(
+        dsn=sentry['dsn'],
+        # Add data like request headers and IP for users,
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+    )
+
+if sentry['tags']:
+    sentry_sdk.set_tags(sentry['tags'])
+
+
+########################################
+### Run sequence
+
+print('-- Downloading namelists...')
+dl_check = dl_nml_domain()
+
+start_date, end_date, hour_interval = check_set_params()
+
+print(f'start date: {start_date}, end date: {end_date}, input hour interval: {hour_interval}')
+
+print('-- Downloading ERA5 data...')
+era5_check = dl_era5(start_date, end_date)
+
+print('-- Processing ERA5 to WPS Int...')
+run_era5_to_int(start_date, end_date, hour_interval)
+
+print('-- Running metgrid.exe...')
+run_metgrid()
+
+print('-- Running real.exe...')
+run_real()
+
+print('-- Running WRF...')
+# monitor_wrf()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- run_wrf.sl
+++ run_wrf.sl
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 # This should be run in the data path
 #SBATCH --job-name=wrf1
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --ntasks=24
 #SBATCH --hint=nomultithread
 
-uv run main.py
+python main_alt.py
 
-n_cores=16
+# n_cores=16
 # wrf_exe_path=$(toml get --toml-path parameters.toml executables.wrf_path)/main/wrf.exe
 
 cd run
 
-module purge 2> /dev/null
+# module purge 2> /dev/null
 # module load netCDF-Fortran/4.6.1-gompi-2024a
-module list
+# module list
 
 # echo $n_cores $wrf_exe_path
 srun --kill-on-bad-exit --output=wrf.log ./wrf.exe
 
-uv run upload_wrfout.py
+python upload_wrfout.py
