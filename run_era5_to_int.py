3a8589da706fae8bdf0362fea7b5f4f03c64e049
Mike Kittridge <mgkittridge@gmail.com>
2025-10-12 17:44:47 +1300

    updated

--- run_era5_to_int.py
+++ run_era5_to_int.py
@@ -29,7 +29,7 @@ def run_era5_to_int(start_date, end_date, hour_interval, del_old=True):
     """
     era5_path = params.data_path.joinpath('era5')
 
-    cmd_str = f'era5_to_int -h {hour_interval} -i {era5_path} "{start_date}" "{end_date}"'
+    cmd_str = f'era5_to_int -h {hour_interval} {era5_path} "{start_date}" "{end_date}"'
     cmd_list = shlex.split(cmd_str)
     p = subprocess.run(cmd_list, capture_output=True, text=True, check=False, cwd=params.data_path)
 
